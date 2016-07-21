4a65e949904934d431a4556e5d833771bcd1800c
tc-mustang <rjl278@cornell.edu>
2016-07-21 14:41:11 -0400

    Filter by imputation accuracy

---
+++ ARfilter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+
+#   Filter a vcf file and outputs only the marker that have a lower AR than the one specified
+#   rjl278@cornell.edu
+#   ARfilter.py <vcf_file> <threshold> <output_prefix>
+
+import sys
+import re
+import gzip
+from itertools import islice
+
+#lista = list()
+#lista_set = set()
+
+######## List with the markers to be on the genetic map file
+
+vcf = sys.argv[1];
+thresh = sys.argv[2];
+out = sys.argv[3];
+
+with gzip.open(vcf, 'rb') as f, gzip.open('%s.vcf.gz' %(out), 'wb') as f_out:
+    
+    head = list(islice(f, 11))
+    for elements in head:
+            f_out.write(elements)
+    
+    for lines in f:
+        ar2 = re.findall("AR2=\d.\d\d", lines[1:350])
+        if ar2[0][4:8] > thresh :
+            f_out.write(lines)
+            
