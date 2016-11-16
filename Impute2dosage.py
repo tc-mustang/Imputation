6b266201b4935ee88f1e9faffb88aa2b4db18b1c
tc-mustang <rjl278@cornell.edu>
2016-11-16 14:23:28 -0500

    Now with filter options for maf and AR2

--- Impute2dosage.py
+++ Impute2dosage.py
@@ -10,13 +10,13 @@ import sys
 import numpy as np
 import gzip
 
-gen = sys.argv[1] #use as script
-sample = sys.argv[2] # use as script
-output = sys.argv[3] 
+gen = sys.argv[1]       # Gen file gzipped
+sample = sys.argv[2]    # Cassava IDs
+filt = sys.argv[3]       # only output these filtered marker
+output = sys.argv[4]    # output will be gzipped
 
 #gen = "output"     #use for testing in the notebook
 #sample = "../final.ped.sample"
-
 
 f1 = gzip.open(output, 'wt')
 
@@ -32,24 +32,35 @@ with open(sample) as g:
     f1.write("\t".join(header))    
     #print ("\t".join(header))
 
-with open(gen) as f :
+keep = list()
+with open(filt) as h:
+    for lines in h:
+        keep.append(lines.rstrip())
+
+keeper = set(keep)
+
+with gzip.open(gen, 'rt') as f :
+    
     for lines in f:
         a = lines.split(" ")
         chrom = a[1].split("-")[0]
         snpid = "S"+str(chrom)+"_"+str(a[2])
         
-        nmarkers = (len(a) - 5)/3
-        markers = a[5:]
+        if snpid in keeper :
+            nmarkers = (len(a) - 5)/3
+            markers = a[5:]
+            
+            dosage = list()
+            
+            for x in range(1, int(nmarkers)*3,3):
+                tmp = float(markers[x-1])*0 + float(markers[x])*1 + float(markers[x+1])*2
+                tmp2 =  '%.2f' % tmp
+                dosage.append(tmp2)
+            
+            dosage.append("\n") 
+            f1.write(snpid+ "\t" + "\t".join(dosage))
         
-        dosage = list()
-        
-        for x in range(1, int(nmarkers)*3,3):
-            tmp = float(markers[x-1])*0 + float(markers[x])*1 + float(markers[x+1])*2
-            tmp2 =  '%.2f' % tmp
-            dosage.append(tmp2)
-        
-        dosage.append("\n") 
-        f1.write(snpid+ "\t" + "\t".join(dosage))
-        #print(snpid+ "\t" + "\t".join(dosage))
+        else:
+            next
 
 f1.close()       
