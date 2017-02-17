aba607bfaa7230e04729065fb6cdf965c323ccb3
tc-mustang <rjl278@cornell.edu>
2017-02-17 15:16:03 -0500

    Rdy to filter dosage files

--- grepy_set.py
+++ grepy_set.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-#   Extract exon number of a set of genes
-#   Depending on the file it would need some tweeks
+#
+#   Filter the Dosage files to only includes SNPs from a list
+#   In general it will look for the first line of a file. If that element is on the list it will print it.
+#   Usage: grepy_set.py <list> <file>
+#   Will print the results to the standard output and the header. Modify if you dont have a header. "File" is gzipped 
 #   rjl278@cornell.edu
-#   LIST + FRequencies
+#
 
 import sys
 import re
-
 import gzip
+
 lista = list()
 lista_set = set()
 
@@ -18,18 +21,17 @@ with open(sys.argv[1]) as f:
     tmp = list()
     for line in f:
         lista.append(line.rstrip())
-        
- 
-      
- ######## Genetic Map file      
 
-lista_set = set(lista)  
-        
-with open(sys.argv[2]) as f:
+######## Genetic Map file
+
+lista_set = set(lista)
+
+with gzip.open(sys.argv[2], 'rb') as f:
+    header = f.readline()
+    print header.rstrip()
     for line in f:
         a = line.split()
-        b = a[3]
-        #print b
+        b = a[0]
         if b in lista_set:
             print line.rstrip()
 
